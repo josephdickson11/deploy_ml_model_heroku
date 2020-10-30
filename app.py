61cb97d07724850bf3b64f65b4b347e88d4c15d9
Ibeun Joseph <josepholadipupoibeun@gmail.com>
2020-10-30 01:29:58 +0100

    starting development of flask app

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+#name of flask app
+app = Flask(__name__)
+
+#run the server
+#this line of code is not needed when running the flask run command in terminal
+#app.run(debug= True)
