9456fef0ff7496102eac603f2d3acd2ca8b3d20f
Ibeun Joseph <josepholadipupoibeun@gmail.com>
2020-10-30 01:46:34 +0100

    adding index page

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
-#name of flask app
+# name of flask app
 app = Flask(__name__)
 
-#run the server
-#this line of code is not needed when running the flask run command in terminal
-#app.run(debug= True)
+
+# run the server
+# this line of code is not needed when running the flask run command in terminal
+# app.run(debug= True)
+
+# defining static app routes
+
+@app.route("/", methods=['GET', 'POST', 'PUT'])  # decorator
+def home():  # route handler function
+    # returning a response
+    return render_template('index.html')
