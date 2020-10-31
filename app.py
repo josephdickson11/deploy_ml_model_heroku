0cb9dd2acee8007808021f2cd2e2207b399a7381
Ibeun Joseph <josepholadipupoibeun@gmail.com>
2020-10-31 12:13:58 +0100

    create predict funtion in app

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for, redirect, jsonify, request
+from pycaret.regression import  *
+import pandas as pd
+import pickle
+import numpy as np
 
 # name of flask app
 app = Flask(__name__)
 
+#create model object
+model = load_model('deployment_model')
+pedictor_col = ['age','sex','bmi','children','smoker','region']
 
 # run the server
 # this line of code is not needed when running the flask run command in terminal
 # app.run(debug= True)
 
-# defining static app routes
 
+# defining static app routes
 @app.route("/", methods=['GET', 'POST', 'PUT'])  # decorator
 def home():  # route handler function
     # returning a response
     return render_template('index.html', name = 'Joseph', gender = 'male')
+
+#create prediction function
+@app.route("/predict", methods=['POST'])
+def predict():
+    int_features = [x for x in request.form.values()]
+    final = np.array(int_features)
+    data_unseen = pd.DataFrame([final], columns=pedictor_col)
+    prediction = predict_model(model, data=data_unseen, round=0)
+    prediction = int(prediction.label[0])
+    return render_template('index.html', pred = 'Expected bill be arounf {}'.format(prediction))
